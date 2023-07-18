756621391ceadce27a44cdcc2b41246921b5648c
Sebastian <seba.faingerch@gmail.com>
2023-07-18 00:08:58 -0300

    feat: create folder helpers an add getHeroes.js inside

--- src/helpers/getHeroes.js
+++ src/helpers/getHeroes.js
@@ -2,13 +2,12 @@ export const getHero = async(hero) =>{
     const url = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=9c0ca35a97ea1851e87a40960f0161bc&hash=88f5cd070cc9490e40ae3ceb77169520&name=${hero}`;
     const resp = await fetch(url);
     const {data} = await resp.json();
-    console.log(data);
+    
 
     const heroes = data.results.map(results=>({
         id: results.id,
         path: results.thumbnail.path,
         extension: results.thumbnail.extension
     }))
-    console.log(heroes);
     return heroes;
 }

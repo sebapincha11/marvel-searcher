30ea18cc20891d54da308bbeacac046d7daf236d
Sebastian <seba.faingerch@gmail.com>
2023-07-18 11:17:08 -0300

    ADD: adding interaction to fav icon

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react";
 import { getHero } from "../helpers/getHeroes";
 import { HeroItem } from "./HeroItem";
-
+import favorito from "../img/favorito.png"
+import favorito_clicked from "../img/favorito_clicked.png"
 
 export const HeroCard = ({hero}) => {
     const [images, setImages] = useState([]);
+    const [currentImg, setCurrentImage] = useState(favorito);
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        if (isClicked) {
+          setCurrentImage(favorito);
+          setIsClicked(false);
+        } else {
+          setCurrentImage(favorito_clicked);
+          setIsClicked(true);
+        }
+      };
 
     const getImages = async() =>{
         const newImages = await getHero(hero);
@@ -18,7 +31,10 @@ export const HeroCard = ({hero}) => {
 
   return (
     <>
-        <h3>{hero}</h3>
+        <div className="title">
+            <h3>{hero}</h3>
+            <img id="fav" src= {currentImg} alt="" onClick={handleClick} />
+        </div>
         <div className="card-grid">
             {
                 images.map((image) => (
@@ -31,6 +47,7 @@ export const HeroCard = ({hero}) => {
                 ))
             }
         </div>
+            
     </>
   )
 }
